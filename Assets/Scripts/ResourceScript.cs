0aa0b58356e789d23d0a3c39fc7cc33502d32e39
Ohmu93 <dekan51@gmail.com>
2016-04-28 17:02:11 +0200

    outline hex and cut off shader

---
+++ Assets/Scripts/ResourceScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceScript : MonoBehaviour {
+
+    private float energy = 0f;
+    private float modifier = 1f;
+
+    public float ChangeEnergyModifier { set { modifier += value; } }
+    public float EnergyModifier { get { return modifier; } set { modifier = value; } }
+    public float Energy { get { return energy; } }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        UpdateEnergy();
+    }
+
+    /// <summary>
+    /// Update current energy level
+    /// </summary>
+    void UpdateEnergy()
+    {
+        if (energy >= 0f && energy < 100f)
+            energy += modifier * Time.deltaTime;
+    }
+}
---
+++ Assets/Shaders/OutlineShader.shader
@@ -0,0 +1,53 @@
+﻿Shader "Unlit/OutlineShader"
+{
+	Properties
+	{
+		_Color ("Color", color) = (1,1,1,1)
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		ztest always
+
+		Stencil
+		{
+			ref 1
+			comp notequal
+			pass keep
+		}
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float4 vertex : POSITION;
+			};
+
+			float4 _Color;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				return o;
+			}
+			
+			float4 frag (v2f i) : SV_Target
+			{
+				return _Color;
+			}
+			ENDCG
+		}
+	}
+}
