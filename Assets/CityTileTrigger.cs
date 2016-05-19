b8d3fa8a4115de06c81a333267b0ab07a32e522b
Ohmu93 <dekan51@gmail.com>
2016-05-19 15:42:40 +0200

    freeze

---
+++ Assets/CityTileTrigger.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CityTileTrigger : MonoBehaviour {
+
+	private bool hasP1Units = false;
+	private bool hasP2Units = false;
+	List<Transform> unitsP1;
+	List<Transform> unitsP2;
+
+	// Use this for initialization
+	void Start () {
+		unitsP1 = new List<Transform>();
+		unitsP2 = new List<Transform>();
+	}
+
+	public void AddUnit(Transform unit, PLAYERS player) {
+		if (player == PLAYERS.PLAYER1)
+			unitsP1.Add (unit);
+		else
+			unitsP2.Add (unit);
+	}
+
+	public void RemoveUnit(Transform unit, PLAYERS player) {
+		if (player == PLAYERS.PLAYER1)
+			unitsP1.Remove (unit);
+		else
+			unitsP2.Remove (unit);
+	}
+
+	public bool HasHostileUnits(PLAYERS player) {
+		if (player == PLAYERS.PLAYER1)
+			return hasP2Units;
+		else
+			return hasP1Units;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col) {
+		AddUnit (col.transform, col.transform.GetComponent<UnitAI> ().Owner);
+		col.GetComponent<UnitAI> ().CityTile = this;
+	}
+}
--- Assets/Scripts/Units/UnitAI.cs
+++ Assets/Scripts/Units/UnitAI.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 
 public class UnitAI : GameManagerSearcher 
 {
+	enum AiState { Idle, Run, Fight, Wander, Cheer };
+	AiState currentState = AiState.Run;
 	[SerializeField]
 	float unitSpeed = 1f;
     [SerializeField]
@@ -13,58 +15,125 @@ public class UnitAI : GameManagerSearcher
 	[SerializeField]
 	float rcDist = 1f;
     private List<Vector3> path;
-
+	[SerializeField]
+	float cheerTime = 1f;
+	[SerializeField]
+	float freezeTime = 1f;
+	private float freezeTimer;
 	[SerializeField]
 	int scoreReward = 10;
 
     private int currentWP = 0;
     private int pathLength;
-	private bool fighting = false;
 	private bool allowCollision = true;
 	private PLAYERS owner;
+	public PLAYERS Owner { get { return owner; } }
 
+	private CityTileTrigger cityTile;
+	public CityTileTrigger CityTile { set { cityTile = value; } }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        Move ();
-		Fight ();
+		PickState();
+		Behave();
     }
 
-	void Move()
-    {
-		if (!fighting && pathLength > currentWP && !HasUnitInFront())
-        {
-			Vector3 dirVec = new Vector3(path[currentWP].x, transform.position.y, path[currentWP].z) - transform.position;
-
-			transform.position += dirVec.normalized * unitSpeed;
-
-			if (dirVec.magnitude <= distanceToWP)
-            {
-                currentWP++;
-				if (pathLength <= currentWP && !fighting)
-                {
-					GenerateScore();
-					Destroy (gameObject);
-                }
-            }
-        }
-    }
-
-	bool HasUnitInFront() {
-		RaycastHit hit;
-		LayerMask layer = owner == PLAYERS.PLAYER1 ? LayerMask.GetMask("UnitP1") : LayerMask.GetMask("UnitP2");
-		return Physics.Raycast(transform.position, (path[currentWP] - transform.position).normalized, rcDist, layer);
+	void PickState() 
+	{
+		bool hasUnitInFront = HasUnitInFront();
+		switch (currentState) {
+		case AiState.Idle:
+			if (freezeTimer > 0f) {
+				freezeTimer -= Time.deltaTime;
+				if (freezeTimer <= 0f)
+					currentState = AiState.Run;
+			}
+			break;
+		case AiState.Run:
+			if (hasUnitInFront)
+				currentState = AiState.Idle;
+			break;
+		case AiState.Fight:
+			break;
+		case AiState.Wander:
+			break;
+		case AiState.Cheer:
+			break;
+		}
 	}
 
-	void Fight() {
-		if (fighting) {
-			animationTime -= Time.deltaTime;
-			if (animationTime <= 0f) {
-				fighting = false;
-				Destroy (gameObject);
-			}
+	void Behave() 
+	{
+		switch (currentState) {
+		case AiState.Idle:
+			Idle ();
+			break;
+		case AiState.Run:
+			Run ();
+			break;
+		case AiState.Fight:
+			Fight ();
+			break;
+		case AiState.Wander:
+			Wander ();
+			break;
+		case AiState.Cheer:
+			Cheer ();
+			break;
 		}
+	}
+
+	void Idle() {
+		
+	}
+
+	void Run()
+    {
+		Vector3 dirVec = new Vector3(path[currentWP].x, transform.position.y, path[currentWP].z) - transform.position;
+
+		transform.position += dirVec.normalized * unitSpeed;
+
+		if (dirVec.magnitude <= distanceToWP)
+			currentWP++;
+		if (pathLength <= currentWP) {
+			GenerateScore ();
+			CustomDestroy ();
+		}
+    }
+
+	void Fight() {
+		/*
+			if (fighting) {
+				animationTime -= Time.deltaTime;
+				if (animationTime <= 0f) {
+					fighting = false;
+					CustomDestroy ();
+				}
+			} else if (cheer) {
+				if (cityTile.HasHostileUnits (owner)) {
+					cheer = false;
+				}
+			}
+			*/
+	}
+
+	void Wander() {
+
+	}
+
+	void Cheer() {
+
+	}
+
+	public void Freeze() {
+		currentState = AiState.Idle;
+		freezeTimer = freezeTime;
+	}
+
+	void CustomDestroy() {
+		Destroy (gameObject);
+		cityTile.RemoveUnit (transform, owner);
 	}
 
 	public void SetData(List<Vector3> unitPath, PLAYERS owner)
@@ -72,7 +141,13 @@ public class UnitAI : GameManagerSearcher
         path = unitPath;
         pathLength = path.Count;
 		this.owner = owner;
-    }
+	}
+
+	bool HasUnitInFront() {
+		RaycastHit hit;
+		LayerMask layer = owner == PLAYERS.PLAYER1 ? LayerMask.GetMask("UnitP1") : LayerMask.GetMask("UnitP2");
+		return Physics.Raycast(transform.position, (path[currentWP] - transform.position).normalized, rcDist, layer);
+	}
 
 	void OnCollisionEnter(Collision col) 
 	{
@@ -81,8 +156,8 @@ public class UnitAI : GameManagerSearcher
 
 		allowCollision = false;
 
-		if (col.transform.GetComponent<UnitAI> ())
-			fighting = true;
+		//if (col.transform.GetComponent<UnitAI> ())
+		//	fighting = true;
 	}
 
 	void GenerateScore()
