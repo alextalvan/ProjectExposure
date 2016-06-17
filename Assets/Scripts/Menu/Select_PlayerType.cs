db79d93a3e8916c96ee6a7b81e52a9f006654660
LeonEsser <PcOne@Leons-MacBook-Pro.local>
2016-06-17 16:56:03 +0200

    Updated Main Menu

---
+++ Assets/Scripts/Menu/Select_PlayerType.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Select_PlayerType : MonoBehaviour {
+
+	public Sprite newSprite;
+	public Button button;
+
+	public void ChangeSprite() 
+	{
+		button.image.sprite = newSprite;
+
+	}
+
+}
