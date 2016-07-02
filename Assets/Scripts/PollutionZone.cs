afad2cb0d47e770210f16cb9bdd91382ec655a6f
Ohmu93 <dekan51@gmail.com>
2016-07-02 12:48:11 +0200

    fixed pollution

--- Assets/Scripts/PollutionZone.cs
+++ Assets/Scripts/PollutionZone.cs
@@ -3,81 +3,81 @@ using System.Collections;
 using System.Collections.Generic;
 
 [RequireComponent(typeof(Collider))]
-public class PollutionZone : MonoBehaviour 
+public class PollutionZone : MonoBehaviour
 {
-	[SerializeField]
-	int currentDamage = 0;
+    [SerializeField]
+    int currentDamage = 0;
 
-	[SerializeField]
-	int maxDamage = 20;
+    [SerializeField]
+    int maxDamage = 20;
 
-	[SerializeField]
-	int turnsBeforeDamageUpgrade = 1;
+    [SerializeField]
+    int turnsBeforeDamageUpgrade = 1;
 
-	[SerializeField]
-	int damageGainPerUpgrade = 1;
+    [SerializeField]
+    int damageGainPerUpgrade = 1;
 
-	int currentTurnCount = 0;
+    int currentTurnCount = 0;
 
-	bool isGrowing = false;
+    bool isGrowing = false;
 
-	[SerializeField]
-	Renderer pollutionDecalRenderer;
+    [SerializeField]
+    Renderer pollutionDecalRenderer;
 
-	[SerializeField]
-	float startPollutionVisibility = 0.25f;
+    [SerializeField]
+    float startPollutionVisibility = 0.25f;
 
 
-	float targetAlpha = 0f;
-	float currentAlpha = 0f;
-	float lerpSpeed = 0.05f;
+    float targetAlpha = 0f;
+    float currentAlpha = 0f;
+    float lerpSpeed = 0.05f;
 
-	public void SetGrowState(bool state)
-	{
-		isGrowing = state;
-	}
+    public void SetGrowState(bool state)
+    {
+        isGrowing = state;
+    }
 
-	public void HandleNewWave()
-	{
-		currentTurnCount++;
-		if(currentTurnCount == turnsBeforeDamageUpgrade)
-		{
-			currentTurnCount = 0;
+    public void HandleNewWave()
+    {
+        currentTurnCount++;
+        if (currentTurnCount == turnsBeforeDamageUpgrade)
+        {
+            currentTurnCount = 0;
 
-			if(isGrowing)
-				currentDamage += damageGainPerUpgrade;
-			else
-				currentDamage -= damageGainPerUpgrade;
+            if (isGrowing)
+                currentDamage += damageGainPerUpgrade;
+            else
+                currentDamage -= damageGainPerUpgrade;
 
-			if(currentDamage < 0)
-				currentDamage = 0;
+            if (currentDamage < 0)
+                currentDamage = 0;
 
-			if(currentDamage > maxDamage)
-				currentDamage = maxDamage;
-            
-			targetAlpha = (float)currentDamage / (float)maxDamage + ((currentDamage > 0) ? startPollutionVisibility : 0.0f);
-			if(targetAlpha > 0.85f)
-				targetAlpha = 0.85f;
-		}
-	}
+            if (currentDamage > maxDamage)
+                currentDamage = maxDamage;
 
-	void OnTriggerEnter(Collider other)
-	{
-		UnitAI ai = other.GetComponent<UnitAI>();
+            targetAlpha = (float)currentDamage / (float)maxDamage + ((currentDamage > 0) ? startPollutionVisibility : 0.0f);
+            if (targetAlpha > 0.85f)
+                targetAlpha = 0.85f;
+        }
+    }
 
-		if(ai)
-		{
-			ai.DecreaseHealth(currentDamage);
-		}
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        UnitAI ai = other.GetComponent<UnitAI>();
 
-	void Update ()
-	{
-		currentAlpha = Mathf.Lerp (currentAlpha, targetAlpha, lerpSpeed);
+        if (ai)
+        {
+            ai.DecreaseHealth(currentDamage);
+        }
+    }
 
-		Color c = pollutionDecalRenderer.material.color;
-		c.a = currentAlpha;
-		pollutionDecalRenderer.material.color = c;
-		pollutionDecalRenderer.material.SetFloat("_Curruption_Clip", currentAlpha*3);
-	}
+    void Update()
+    {
+        currentAlpha = Mathf.Lerp(currentAlpha, targetAlpha, lerpSpeed);
+
+        Color c = pollutionDecalRenderer.material.color;
+        c.a = currentAlpha;
+        pollutionDecalRenderer.material.color = c;
+        pollutionDecalRenderer.material.SetFloat("_Curruption_Clip", 1 - currentAlpha);
+    }
 }
