88a215fe860882bd055ec0f7ba343b393144dc1f
Ohmu93 <dekan51@gmail.com>
2016-07-03 23:54:26 +0200

    б

--- Assets/ArrowScript.cs
+++ Assets/ArrowScript.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class ArrowScript : MonoBehaviour {
 
     [SerializeField]
-    float lerpSpeed = 0.01f;
+    float lerpSpeed = 1f;
 
     [SerializeField]
     GameObject hostCard;
@@ -23,8 +23,8 @@ public class ArrowScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (hostCard.transform.parent.position == hostCard.transform.position && targetCard.transform.parent.position == targetCard.transform.position)
-            img.color = new Color(1f, 1f, 1f, Mathf.Lerp(img.color.a, 1f, Time.time * lerpSpeed));
+            img.color = new Color(1f, 1f, 1f, Mathf.Lerp(img.color.a, 1f, lerpSpeed));
         else
-            img.color = new Color(1f, 1f, 1f, Mathf.Lerp(img.color.a, 0f, Time.time * lerpSpeed));
+            img.color = new Color(1f, 1f, 1f, Mathf.Lerp(img.color.a, 0f, lerpSpeed));
     }
 }
--- Assets/Scripts/FPSDisplay.cs
+++ Assets/Scripts/FPSDisplay.cs
@@ -3,42 +3,51 @@
 
 public class FPSDisplay : MonoBehaviour
 {
-	float timeAccumulator;
-	int frameRate = 0;
-	int count;
+    float timeAccumulator;
+    int frameRate = 0;
+    int count;
+    bool enabled = false;
 
-	void Update()
-	{
-		count++;
-		timeAccumulator += Time.deltaTime;
+    void Update()
+    {
+        if (!enabled && Input.GetKeyDown(KeyCode.F))
+            enabled = true;
+        else if (enabled && Input.GetKeyDown(KeyCode.F))
+            enabled = false;
 
-		if(timeAccumulator >= 1.0f)
-		{
-			timeAccumulator -= 1.0f;
-			frameRate = count;
-			count = 0;
-		}
-	}
+        count++;
+        timeAccumulator += Time.deltaTime;
 
-	void Start()
-	{
-		timeAccumulator = 0;
-		count = 0;
-	}
+        if (timeAccumulator >= 1.0f)
+        {
+            timeAccumulator -= 1.0f;
+            frameRate = count;
+            count = 0;
+        }
+    }
 
-	void OnGUI()
-	{
-		int w = Screen.width, h = Screen.height;
+    void Start()
+    {
+        timeAccumulator = 0;
+        count = 0;
+    }
 
-		GUIStyle style = new GUIStyle();
+    void OnGUI()
+    {
+        if (enabled)
+        {
+            int w = Screen.width, h = Screen.height;
 
-		Rect rect = new Rect(0, 0, w, h * 2 / 100);
-		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h * 2 / 100;
-		style.normal.textColor = new Color (0.0f, 0.0f, 0.5f, 1.0f);
-		//float fps = 1.0f / deltaTime;
-		//string text = string.Format("({0:0.} fps)", frameRate);
-		string text = "FPS: " + frameRate.ToString();
-		GUI.Label(rect, text, style);
-	}
+            GUIStyle style = new GUIStyle();
+
+            Rect rect = new Rect(0, 0, w, h * 2 / 100);
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = h * 2 / 100;
+            style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+            //float fps = 1.0f / deltaTime;
+            //string text = string.Format("({0:0.} fps)", frameRate);
+            string text = "FPS: " + frameRate.ToString();
+            GUI.Label(rect, text, style);
+        }
+    }
 }
